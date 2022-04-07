e62b2ca75bfddc248093af7ef499aad27b4c8d1c
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2022-04-07 17:09:59 +0700

    show detail profile foto

--- resources/views/partials/side.blade.php
+++ resources/views/partials/side.blade.php
@@ -34,12 +34,12 @@
 
     <div class="row sideBar">
         <div class="row sideBar-body" onclick="chat(1)">
-            <div class="col-sm-3 col-xs-1 sideBar-avatar">
+            <div class="col-sm-3 col-xs-2 sideBar-avatar">
                 <div class="avatar-icon">
                     <img src="https://bootdey.com/img/Content/avatar/avatar1.png">
                 </div>
             </div>
-            <div class="col-sm-9 col-xs-11 sideBar-main">
+            <div class="col-sm-9 col-xs-10 sideBar-main">
                 <div class="row">
                     <div class="col-sm-8 col-xs-8 sideBar-name">
                         <span class="name-meta">John Doe
@@ -112,35 +112,29 @@
         </div>
     </div>
 
-    <div class="row composeBox">
-        <div class="col-sm-12 composeBox-inner">
-            <div class="form-group has-feedback">
-                <input id="composeText" type="text" class="form-control" name="searchText"
-                    placeholder="Search People">
-                <span class="glyphicon glyphicon-search form-control-feedback"></span>
+    <div class="row compose-sideBar">
+        <div class="row">
+            <div class="col-sm-12 col-xs-12">
+                <div class="avatar-icon-profile text-center">
+                    <br>
+                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Profile foto" type="button"
+                        data-toggle="modal" data-target="#modal-profile-foto">
+                </div>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="row compose-sideBar">
-        <div class="row sideBar-body">
-            <div class="col-sm-3 col-xs-3 sideBar-avatar">
-                <div class="avatar-icon">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png">
-                </div>
-            </div>
-            <div class="col-sm-9 col-xs-9 sideBar-main">
-                <div class="row">
-                    <div class="col-sm-8 col-xs-8 sideBar-name">
-                        <span class="name-meta">John Doe
-                        </span>
-                    </div>
-                    <div class="col-sm-4 col-xs-4 pull-right sideBar-time">
-                        <span class="time-meta pull-right">18:18
-                        </span>
-                    </div>
-                </div>
-            </div>
+<div class="modal fade" id="modal-profile-foto" tabindex="-1" role="dialog" aria-labelledby="profile-label"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <button type="button" class="close btn-close-profile-foto" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <div class="text-center">
+            <br><br>
+            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Profile foto"
+                class="profile-foto-detail">
         </div>
     </div>
 </div>
